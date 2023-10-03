0e1f88ceb1c8eaf739841dc2a1c8cf96d61be3a5
Benedikt Weis <benedikt.georg.weis@gmail.com>
2023-10-03 21:59:41 +0200

    Added a Repository for HeadFirst Book Exercises

--- HeadFirst_Java/MockSongs.java
+++ HeadFirst_Java/MockSongs.java
@@ -5,12 +5,12 @@ class MockSongs {
 
     public static List<SongV3> getSongsV3() {
         List<SongV3> songs = new ArrayList<>();
-        songs.add(new SongV2("somersault", "zero 7", 147));
-        songs.add(new SongV2("cassidy", "grateful dead", 158));
-        songs.add(new SongV2("$10", "hitchhiker", 140));
-        songs.add(new SongV2("havana", "cabello", 105));
-        songs.add(new SongV2("Cassidy", "grateful dead", 158));
-        songs.add(new SongV2("50 ways", "simon", 102));
+        songs.add(new SongV3("somersault", "zero 7", 147));
+        songs.add(new SongV3("cassidy", "grateful dead", 158));
+        songs.add(new SongV3("$10", "hitchhiker", 140));
+        songs.add(new SongV3("havana", "cabello", 105));
+        songs.add(new SongV3("Cassidy", "grateful dead", 158));
+        songs.add(new SongV3("50 ways", "simon", 102));
         return songs;
         // We made a new method in the MockSongs class to mock the new song data.
     }

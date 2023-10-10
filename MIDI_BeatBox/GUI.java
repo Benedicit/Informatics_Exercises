e61fbb42a036cfc88ea6d7f8a37f3765c35ba0a9
Benedikt Weis <benedikt.georg.weis@gmail.com>
2023-10-10 14:53:22 +0200

    Updated it

--- MIDI_BeatBox/GUI.java
+++ MIDI_BeatBox/GUI.java
@@ -1,6 +1,20 @@
 package MIDI_BeatBox;
 import javax.swing.*;
+import java.awt.event.*;
 
-public class GUI {
+public class GUI implements ActionListener {
+    private JButton button;
+    public void go() {
+        JFrame frame = new JFrame();
+        button = new JButton("click me");
+        button.addActionListener(this);
 
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().add(button);
+        frame.setSize(300, 300);
+        frame.setVisible(true);
+    }
+    public void actionPerformed(ActionEvent event) {
+        button.setText("I've been clicked");
+    }
 }
